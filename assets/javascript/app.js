13a83636f621544ce775f7fc702b91efee8dae22
HelioSchneiderr <helioschndr@gmail.com>
2022-05-09 23:53:18 -0300

    images and scripts

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,47 @@
-const names = document.getElementById(`name`);
-const forc = document.getElementById(`forc`);
-const int = document.getElementById(`int`);
-const des = document.getElementById(`name`);
-const person = document.getElementById(`person`);
+
 const btnCreate = document.querySelector(".butonCreate");
 const questions = document.querySelector("#questions");
 const title = document.querySelector(".textTitle")
+const textP = document.querySelector(".textP")
 
 
-btnCreate.addEventListener("click", ()=>{
-    questions.style.display = `none`;
-    questions.classList.add(`julio`);
-    title.textContent = " Your Character"
+
+
+btnCreate.addEventListener("click", () => {
+
+    //INPUTS
+    const names = document.getElementById(`name`).value;
+    const forc = parseInt(document.getElementById(`forc`).value);
+    const int = parseInt(document.getElementById(`int`).value);
+    const des = parseInt(document.getElementById(`des`).value);
+    const person = parseInt(document.getElementById(`person`).value);
+
+    //0 to 10, condition
+    if (forc <= 10 && des <= 10 && int <= 10 && person <= 10) {
+        
+        //style none
+        questions.style.display = `none`;
+        questions.classList.add(`julio`);
+        
+        //Final Text
+        title.textContent = " Your Character";
+        textP.textContent = `Your character is called ${names} has ${forc} strength, ${des} dexterity, ${int} intelligence, and he sure is a guy ${person}`;
+        
+        //Final img
+        let img = document.createElement("img");
+        img.src=`../../imagens/avatar${numberRandom(1, 5)}.png`;
+        textP.appendChild(img);
+
+        
+
+        
+    } else {
+        alert("Some field is wrong")
+    }
     
-    //values
-    let name = names.value;
-    let force = forc.value;
-    let inter = int.value;
-    let dest = des.value;
-    let perso = person.value;
-
-    if(force > 10){
-        forc.value = 0;
-    };
-
-    let character = `Your character is called ${name} has ${force} strength, ${dest} dexterity, ${inter} intelligence, and he sure is a guy ${perso}`
-    
-    console.log(character)
-
-
-    
-
-
-
-    //Your character is called ${} has ${} strength, ${} dexterity, ${} intelligence, and he sure is a guy ${};
 });
 
+
+function numberRandom(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
